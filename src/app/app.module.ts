b1d8d67f7b8f693f40538b10477374f14d326b8a
andymwamengo <andymwamengo@gmail.com>
2021-07-25 18:23:22 +0300

    UPDATED the routing strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes, ExtraOptions } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
 import { ProfileModule } from './profile/profile.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
