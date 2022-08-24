2988182280214f7652495d8c4f53b53b071e9ae7
andymwamengo <andymwamengo@gmail.com>
2022-08-24 23:46:07 +0300

    fix: added the icons | dynamic template

--- src/app/model/contact.ts
+++ src/app/model/contact.ts
@@ -9,6 +9,7 @@ export interface DataObject {
   label: string,
   link: string,
   name: string,
+  icon: string,
 }
 
 export const CONTACT: Contact = {
@@ -19,30 +20,57 @@ export const CONTACT: Contact = {
       label: "Email",
       link: "mailto:andymwamengo@gmail.com",
       name: "andymwamengo@gmail.com",
-    },
-    {
-      id: '2',
-      label: "Website",
-      link: "https://andymwamengo.com",
-      name: "https://andymwamengo.com",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="24" height="24"
+      viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+      stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <rect x="3" y="5" width="18" height="14" rx="2"></rect>
+      <polyline points="3 7 12 13 21 7"></polyline>
+    </svg>`,
     },
     {
       id: '3',
       label: "LinkedIn",
       link: "https://www.linkedin.com/in/andymwamengo/",
       name: "https://www.linkedin.com/in/andymwamengo/",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="24"
+      height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+      stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <rect x="4" y="4" width="16" height="16" rx="2"></rect>
+      <line x1="8" y1="11" x2="8" y2="16"></line>
+      <line x1="8" y1="8" x2="8" y2="8.01"></line>
+      <line x1="12" y1="16" x2="12" y2="11"></line>
+      <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+    </svg>`,
     },
     {
       id: '4',
       label: "Twitter",
       link: "https://twitter.com/andymwamengo",
       name: "https://twitter.com/andymwamengo",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24"
+      height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+      stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path>
+      <line x1="10" y1="14" x2="20" y2="4"></line>
+      <polyline points="15 4 20 4 20 9"></polyline>
+    </svg>`,
     },
     {
       id: '5',
       label: "GitHub",
       link: "https://github.com/andymwamengo",
       name: "https://github.com/andymwamengo",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="24"
+      height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+      stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path
+        d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z">
+      </path>
+    </svg>`,
     },
   ]
 };
